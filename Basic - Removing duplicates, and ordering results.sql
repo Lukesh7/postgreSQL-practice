e2af9759de2079c026435dc6addb7bd462cc101f
Lukesh M <lukeshmurugesan@gmail.com>
2026-07-06 23:17:45 +0530

    Create SQL query for unique sorted surnames
    Add SQL query to retrieve distinct ordered surnames

---
+++ Basic - Removing duplicates, and ordering results.sql
@@ -0,0 +1,7 @@
+How can you produce an ordered list of the first 10 surnames 
+in the members table? The list must not contain duplicates.
+
+select distinct surname
+from cd.members
+order by surname asc
+limit 10;
